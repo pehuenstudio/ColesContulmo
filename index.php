be37b77026a22bddfc324c24d8bbd13b03a26a26
rodrigosecastro <rodrigo.secastro@gmail.com>
2015-04-01 13:20:20 -0300

    v0.3

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
 echo "<br/>".dirname(__FILE__)."\\".basename(__FILE__)."<br/>";
-require_once $_SERVER["DOCUMENT_ROOT"]."/modelos/Usuarios/Alumno.php";
+require_once $_SERVER["DOCUMENT_ROOT"]."/modelos/Usuarios/Profesor.php";
+
+$profesor = new Profesor();
+$profesor->validar_run();
 ?>
--- modelos/Usuarios/Profesor.php
+++ modelos/Usuarios/Profesor.php
@@ -1,4 +1,10 @@
 <?php
 echo "<br/>".dirname(__FILE__)."\\".basename(__FILE__)."<br/>";
 
+require_once($_SERVER["DOCUMENT_ROOT"]."/modelos/Usuarios/Usuario.php");
+
+class Profesor extends Usuario{
+
+}
+
 ?>

1f57b941f4a72b9eaad510c4bfe124065c2f5629
rodrigosecastro <rodrigo.secastro@gmail.com>
2015-04-02 14:37:06 -0300

    v0.11 conexion a BBDD exitosa

--- config.php
+++ config.php
@@ -1,6 +1,32 @@
 <?php
 echo __FILE__."<br/>";
 
+//DEFINICION DE CONSTANTES
 define("ROOT_MODELOS",$_SERVER["DOCUMENT_ROOT"]."/_code/modelos/");
 define("ROOT_MODELOS_USUARIOS",$_SERVER["DOCUMENT_ROOT"]."/_code/modelos/usuarios/");
+
+//echo $_SERVER["SERVER_NAME"]."<br/>";
+
+if ($_SERVER["SERVER_NAME"] == "colescontulmo.local"){
+    //echo "ESTAS EN LOCALHOST"; //DATOS BBDD LOCAL
+    define("DB_HOST","localhost");//NOMBRE DEL SERVIDOR
+    define("DB_NAME","coles_data");//NOMBRE DE LA BBDD
+    define("DB_USER","coles_data");//NOMBRE DE USUARIO DE BBDD
+    define("DB_PASS","20150402");//CONTRASEÑA DE BBDD
+}else{
+    //echo "ESTAS EN LOCALHOST"; //DATOS BBDD WEB
+    define("DB_HOST","mysql.hostinger.es");//NOMBRE DEL SERVIDOR
+    define("DB_NAME","u906616672_data");//NOMBRE DE LA BBDD
+    define("DB_USER","u906616672_data");//NOMBRE DE USUARIO DE BBDD
+    define("DB_PASS","20150402");//CONTRASEÑA DE BBDD
+}
+
+try{
+    $myPDO = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER,DB_PASS);
+    //echo "<br/>ÉXITO EN LA CONEXIÓN BBDD<BR/>";
+
+}catch(PDOException $e){
+    print "<br/>ERROR: ".$e->getMessage()." <br/>";
+    die();
+}
 ?>
--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 echo __FILE__."<br/>";
 require_once $_SERVER["DOCUMENT_ROOT"]."/_code/config.php";
-require_once ROOT_MODELOS_USUARIOS."Profesor.php";
+/*require_once ROOT_MODELOS_USUARIOS."Profesor.php";
 require_once ROOT_MODELOS_USUARIOS."Apoderado.php";
 require_once ROOT_MODELOS_USUARIOS."Alumno.php";
-
+/
 $profesor = new Profesor();
 $apoderado = new Apoderado();
 $alumno = new Alumno();
@@ -12,4 +12,6 @@ $alumno = new Alumno();
 var_dump($profesor);
 var_dump($apoderado);
 var_dump($alumno);
+*/
+
 ?>
